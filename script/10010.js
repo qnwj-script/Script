db317d8f4bf3894aa842010c8016fec68ddce6da
Neo <neo@matrix.com>
2025-03-05 21:33:13 +0800

    init

--- script/10010.js
+++ script/10010.js
@@ -1,30 +1,6 @@
-/*
-
-获取方式：打开  中国联通 app 【官方版】-> 首页的流量查询获取 Cookie
-===================
-[MITM]
-hostname = m.client.10010.com
-
-【Surge脚本配置】:
-===================
-[Script]
-联通组件 = type=http-request,pattern=https:\/\/m\.client\.10010\.com\/(.*)\/smartwisdomCommon,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/dompling/Script/master/10010/index.js,script-update-interval=0
-
-===================
-【Loon脚本配置】:
-===================
-[Script]
-http-request https:\/\/m\.client\.10010\.com\/(.*)\/smartwisdomCommon tag=联通 headers, script-path=https://raw.githubusercontent.com/dompling/Script/master/10010/index.js
-
-===================
-【 QX  脚本配置 】 :
-===================
-
-[rewrite_local]
-https:\/\/m\.client\.10010\.com\/(.*)\/smartwisdomCommon  url script-request-header https://raw.githubusercontent.com/dompling/Script/master/10010/index.js
-
+/**
+ * 获取方式：打开  中国联通 app 【官方版】-> 首页的流量查询获取 Cookie
  */
-
 const APIKey = 'YaYa_10010';
 $ = new API(APIKey, true);
 if ($request) GetCookie();
